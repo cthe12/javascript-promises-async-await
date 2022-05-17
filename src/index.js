35f7212e66c4697e4696a90576d3c41407a6b56b
Chantel T <chantel.theodule@outlook.com>
2022-05-17 18:48:36 -0400

    initialize moviePromise

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const movies = require("./data/movies.json")
 
 export function fetchMovies() {
     const resolveFunction = () => movies
-    fetchWithTimeout(1000).then(resolveFunction)
+    return fetchWithTimeout(1000).then(resolveFunction)
 }
-moviePromise = fetchMovies()
+const moviePromise = fetchMovies()
 moviePromise.then(results =>
 console.log(results))
