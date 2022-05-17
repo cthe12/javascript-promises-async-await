b01e02e5c08f7a157a5d00dffabda172539ee51a
Chantel T <chantel.theodule@outlook.com>
2022-05-17 19:05:57 -0400

    run multiple promises

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+import { fetchMovies } from "./services"
+import { fetchBooks } from "./services"
 import { fetchWithTimeout } from "./services"
 const movies = require("./data/movies.json")
 
-export function fetchMovies() {
-    const resolveFunction = () => movies
-    return fetchWithTimeout(1000).then(resolveFunction)
+function getBooksandMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies ]) => ({
+        books,
+        movies
+      }))
+      .catch(error => console.log("Error fetching books and movies", error))
 }
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-  console.log(results)
-})
+
+const getBooksAndMoviesPromise = getBooksandMovies()
+getBooksAndMoviesPromise.then( results => console.log('getBooksAndMoviesPromise', results))
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+    .then(results => results)
+.catch(error => console.log("Error waiting for the promise race", error))
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies()
+getBooksAndMoviesPromise.then( results => console.log('getBooksOrMoviesPromise', results))
