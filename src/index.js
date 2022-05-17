39f33f8421d86bcf4fa645131a58928f09a0fc3c
Chantel T <chantel.theodule@outlook.com>
2022-05-17 18:50:07 -0400

    log results

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export function fetchMovies() {
     const resolveFunction = () => movies
     return fetchWithTimeout(1000).then(resolveFunction)
 }
-const moviePromise = fetchMovies()
-moviePromise.then(results =>
-console.log(results))
+const moviePromise = fetchMovies();
+moviePromise.then(results => {
+  console.log(results)
+})
